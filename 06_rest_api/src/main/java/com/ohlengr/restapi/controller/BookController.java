97bd7f2c6447034894b50bf53fc13f11339dba41
SATNAM SINGH <ohlengr@gmail.com>
2026-09-26 10:42:12 +0530

    feat: add book lookup by path variable

--- 06_rest_api/src/main/java/com/ohlengr/restapi/controller/BookController.java
+++ 06_rest_api/src/main/java/com/ohlengr/restapi/controller/BookController.java
@@ -3,6 +3,7 @@ package com.ohlengr.restapi.controller;
 import com.ohlengr.restapi.model.Book;
 import com.ohlengr.restapi.service.BookService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,10 @@ public class BookController {
     @GetMapping("/books")
     public List<Book> getBooks(){
         return bookService.getAllBooks();
+    }
+
+    @GetMapping("/books/{id}")
+    public Book getBookById(@PathVariable long id){
+        return bookService.getBookById(id);
     }
 }
--- 06_rest_api/src/main/java/com/ohlengr/restapi/service/BookService.java
+++ 06_rest_api/src/main/java/com/ohlengr/restapi/service/BookService.java
@@ -8,10 +8,21 @@ import java.util.List;
 
 @Service
 public class BookService {
-    public List<Book> getAllBooks(){
-        List<Book> books = new ArrayList<>();
+    List<Book> books = new ArrayList<>();
+    public BookService(){
         books.add(new Book(1,"Clean Code","Robert C. Martin"));
         books.add(new Book(2,"Effective Java","Joshua Bloch"));
+    }
+
+    public List<Book> getAllBooks(){
         return books;
+    }
+    public Book getBookById(long id){
+        for(Book book: books){
+            if(book.getId()==id) {
+                return book;
+            }
+        }
+        return null;
     }
 }
